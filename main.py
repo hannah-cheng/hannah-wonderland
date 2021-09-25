a3923f66978d3a3aefc8fb72824e5148602a8e40
howard2448839 <howard2448839@gmail.com>
2021-09-25 14:55:41 +0800

    init

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import flask
+# import flask
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+# app = flask.Flask(__name__)
+# app.config["DEBUG"] = True
 
 
-@app.route('/', methods=['GET'])
+# @app.route('/', methods=['GET'])
+# def home():
+#     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+# app.run()
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Welcome to Hannah :)</h1>"
 
-app.run()
+if __name__ == "__main__":
+    app.run()
